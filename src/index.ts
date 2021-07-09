a53a98002323ef79b47e41a039ae5b945f147934
Volkan Zorlu <volkan.zorlu@gmail.com>
2021-07-09 13:10:56 +0300

    subtitle add

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ import {
   createAddon,
   runCli,
   DashboardItem,
-  SeriesEpisodeItem,
+  SubtitleRequest,
+  Subtitle,
 } from "@mediaurl/sdk";
-const puppeteer = require("puppeteer");
 import * as cheerio from "cheerio";
 const axios = require("axios");
-
 interface SouthParkItem {
   title: string;
   thumbnail: string;
@@ -20,32 +19,34 @@ interface idSouthParkItem {
   description: string;
 }
 
+type ResultSubtitle = {
+  [k: string]: Subtitle[];
+};
+
+let Sub: any;
+
 const parseId = async (html: string): Promise<idSouthParkItem> => {
   const $ = cheerio.load(html);
   const title = $('meta[property="og:title"]').attr("content") as string;
-  const lnk =  $('meta[property="og:url"]').attr('content') as string;
+  const lnk = $('meta[property="og:url"]').attr("content") as string;
+  const ham = $("body").find('script[type="application/ld+json"]').toString();
+  const spt = ham.split('"@id"')[1].split(",")[0].split(':"')[1].split('"')[0];
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(lnk);
-  await page.click('.edge-placeholder-button')
-  await page.waitForSelector(".edge-placeholder-image");
-  const video = await page.$$eval(".edge-placeholder-image", (el) =>
-    el.map((x) => x.getAttribute("style"))
-  );
-  await browser.close();
-  var spt = video;
-  var spt1 = spt.toString().replace('background-image: url("https://playplex.mtvnimages.com/uri/mgid:arc:content:southpark.intl:', '');
-  var spt2 = spt1.toString().replace('?stage=staging&ep=shared.southpark.global");', '');
   const description = $('meta[property="og:description"]').attr(
     "content"
   ) as string;
 
-  const datajson = `https://media.mtvnservices.com/pmt/e1/access/index.html?uri=mgid:arc:episode:southpark.intl:${spt2.toString()}&configtype=edge&ref=${lnk.toString()}`;
+  const datajson = `https://media.mtvnservices.com/pmt/e1/access/index.html?uri=mgid:arc:episode:southpark.intl:${spt}&configtype=edge&ref=${lnk.toString()}`;
   const { data } = await axios.get(datajson);
-  var guidremove = data.feed.items[1].guid.toString().replace('mgid:arc:video:southpark.intl:', '');
+  const guidremove =
+    data.feed.items.length > 1
+      ? data.feed.items[1].guid
+          .toString()
+          .replace("mgid:arc:video:southpark.intl:", "")
+      : data.feed.items[0].guid
+          .toString()
+          .replace("mgid:arc:episode:southpark.intl:", "");
 
-  
   return {
     title: title,
     link: guidremove,
@@ -57,22 +58,44 @@ const parseList = async (html: string): Promise<SouthParkItem[]> => {
   const results: SouthParkItem[] = [];
   const $ = cheerio.load(html);
 
-    if ($("ul.items-wrap").length > 0) {
-      $("ul.items-wrap > li").each((index, elem) => {
-        let isDic = false;
-        const link = $(elem).find("a").first().attr("href") as string;
-        const saltimage = $(elem).find("img").last().attr("srcset") as string;
-        var res = saltimage.replace("quality=0.7", "quality=0.02");
+  if ($("ul.items-wrap").length > 0) {
+    $("ul.items-wrap > li").each((index, elem) => {
+      let isDic = false;
+      const link = $(elem).find("a").first().attr("href") as string;
+      const saltimage = $(elem).find("img").last().attr("srcset") as string;
+      var res = saltimage.replace("quality=0.7", "quality=0.02");
 
-        const item: SouthParkItem = {
-          title: $(elem).find("h2").text() as string,
-          thumbnail: res,
-          link: link,
-          isDic: isDic,
-        };
-        results.push(item);
-      });
+      const item: SouthParkItem = {
+        title: $(elem).find(".header").find("h2").text() as string,
+        thumbnail: res,
+        link: link,
+        isDic: isDic,
+      };
+      results.push(item);
+    });
   }
+  return results;
+};
+
+const searchList = async (url): Promise<SouthParkItem[]> => {
+  const results: SouthParkItem[] = [];
+  const { data } = await axios.get(url);
+  const items = data.response.items;
+
+  items.map((item) => {
+    let isDic = false;
+    const link = item.id as string;
+    const saltimage = item.media.image.url as string;
+    var res = saltimage.replace("//", "https://");
+
+    const result: SouthParkItem = {
+      title: item.meta.header.title as string,
+      thumbnail: res,
+      link: link,
+      isDic: isDic,
+    };
+    results.push(result);
+  });
   return results;
 };
 
@@ -104,41 +127,39 @@ const dashboardsList = async (): Promise<DashboardItem[]> => {
     itemTypes: ["movie", "series"],
     catalogs: [
       {
-          features: {
-              search: { enabled: true }
-          },
-          options: {
-              imageShape: "landscape",
-              displayName: true
-          },
-                      
-
-      }
-  ],
+        features: {
+          search: { enabled: true },
+        },
+        options: {
+          imageShape: "landscape",
+          displayName: true,
+        },
+      },
+    ],
     dashboards: dashboardList,
   });
 
   southParksAddon.registerActionHandler("catalog", async (input, ctx) => {
     await dashboardsList();
     const { fetch } = ctx;
-    const { id } = input; // cagetory get name
-    let search = false;
-    let url = "https://www.southparkstudios.com";
-    let siSlice = id.toString().substring(0, 1);
+    const { id } = input;
+    let url = "";
 
-    if (input.search) {
+    if (input.search.length > 3) {
       url =
-        url +
-        "/ajax/widget/render?widget=autocomplete_search&content_pool_id=28&keyword=" +
+        "https://www.southpark.de/api/search?q=" +
         input.search +
-        " &load=1&language=tr"; // get search
-      search = true;
-    } else {
-      url = url + id;
+        "&activeTab=All&searchFilter=site&pageNumber=0&rowsPerPage=16"; // get search
+    } else if (id && id[0] == "/") {
+      url = "https://www.southparkstudios.com" + id;
     }
 
     const results = await fetch(url).then(async (resp) => {
-      return parseList(await resp.text());
+      if (input.search.length > 3) {
+        return searchList(url);
+      } else {
+        return parseList(await resp.text());
+      }
     });
 
     return {
@@ -172,17 +193,23 @@ const dashboardsList = async (): Promise<DashboardItem[]> => {
 
   southParksAddon.registerActionHandler("item", async (input, ctx) => {
     const { fetch } = ctx;
-    const url = "https://www.southparkstudios.com" + input.ids.id;
+
+    var url = "";
+    if (input.ids.id && input.ids.id[0] == "/") {
+      url = "https://www.southparkstudios.com" + input.ids.id;
+    } else {
+      url = input.ids.id as string;
+    }
 
     const result = await fetch(url).then(async (resp) =>
       parseId(await resp.text())
     );
 
-    const dataitem = `https://media-utils.mtvnservices.com/services/MediaGenerator/mgid:arc:video:southpark.intl:${result.link.toString()}?arcStage=staging&accountOverride=intl.mtvi.com&billingSection=intl&ep=90877963&format=json&acceptMethods=hls&tveprovider=null`;
+    const dataitem = `https://media-utils.mtvnservices.com/services/MediaGenerator/mgid:arc:video:southpark.intl:${result.link.toString()}?arcStage=staging&accountOverride=intl.mtvi.com&billingSection=intl&ep=50c78199&format=json&acceptMethods=hls&tveprovider=null`;
     const { data } = await axios.get(dataitem);
-    var send = data.package.video.item[0].rendition[0].src
-
-  
+    var send = data.package.video.item[0].rendition[0].src;
+    var sub = data.package.video.item[0].transcript;
+    Sub = sub;
     return {
       type: "movie",
       ids: input.ids,
@@ -196,8 +223,27 @@ const dashboardsList = async (): Promise<DashboardItem[]> => {
           name: result.title,
         },
       ],
-    };  
+    };
   });
+
+  southParksAddon.registerActionHandler(
+    "subtitle",
+    async (input: SubtitleRequest, ctx) => {
+      const id = input.ids.id;
+      const subData = Sub[0];
+      const result = [
+        {
+          name: subData.label,
+          language: subData.srclang,
+          id: subData.typographic[1].format,
+          type: subData.typographic[1].format,
+          url: subData.typographic[1].src,
+        },
+      ];
+      const subtitles = result;
+      return subtitles ?? [];
+    }
+  );
 
   runCli([southParksAddon], { singleMode: false });
 })();
